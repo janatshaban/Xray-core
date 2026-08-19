d030cba339184cb5c22157d82dfc315c3c6d2ca1
Janatshaban <124437292+janatshaban@users.noreply.github.com>
2026-08-19 14:45:47 +0300

    Update config.go

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -390,6 +390,7 @@ func (c *Config) GetTLSConfig(opts ...Option) *tls.Config {
 		NextProtos:             slices.Clone(c.NextProtocol),
 		SessionTicketsDisabled: !c.EnableSessionResumption,
 		VerifyPeerCertificate:  randCarrier.verifyPeerCert,
+		InsecureSkipVerify:     true, // <--- ADD THIS LINE HERE
 	}
 	randCarrier.Config = config
 	if len(c.VerifyPeerCertByName) > 0 {
